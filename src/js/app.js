5f31713b73b5babda0dca81fcc913b728bfad321
Vitali Brych <vbrych@cpaglobal.com>
2019-12-14 00:08:52 +0300

    feat: add checking

--- src/js/app.js
+++ src/js/app.js
@@ -1,58 +1,92 @@
-import 'babel-polyfill';
-window.onload = function() {
-  alert('Страница загружена');
+// import 'babel-polyfill';
+function checkValid() {
+  let arrayField = [];
+
+  document.querySelectorAll('input').forEach(item => {          
+    if (item.required && !item.value) {
+      const nameValue = item.previousSibling.previousElementSibling.innerText;
+      arrayField.push(nameValue)
+    }
+  });
+
+  document.querySelectorAll('select').forEach(item => {          
+    if (item.required && !item.value) {
+      const nameValue = item.previousSibling.previousElementSibling.innerText;
+      arrayField.push(nameValue)
+    }
+  });
+
+  let retiree = document.querySelectorAll('#retiree');
   
- 
+  if (retiree[0].checked && retiree[1].checked) {
+    alert('Выберете одно значение в поле Пенсионер');
+    return false;
+  } else if ((!retiree[0].checked && !retiree[1].checked)) {
+    arrayField.push('Пенсионер');
+  }  
+  
+  if (arrayField.length) {
+    alert(`Заполните поля ${arrayField.join(', ')}`)
+    return false
+  }
+
+  return true;
+}
+
+window.onload = function() {
+  alert('Страница загружена'); 
 
   async function getRecords() {
 
-    const userData = {
-      family: `${document.querySelector('#family').value}`,
-      name: `${document.querySelector('#name').value}`,
-      secondName: `${document.querySelector('#secondName').value}`,
-      dateBorn: `${document.querySelector('#dateBorn').value}`,
-  
-      seriaPasp: `${document.querySelector('#seriaPasp').value}`,
-      numPasp: `${document.querySelector('#numPasp').value}`,
-      whoDone: `${document.querySelector('#whoDone').value}`,
-      dateDone: `${document.querySelector('#dateDone').value}`,
-      idPasp: `${document.querySelector('#idPasp').value}`,
-  
-      placeBorn: `${document.querySelector('#placeBorn').value}`,
-      city: `${document.querySelector('#city').value}`,
-      adress: `${document.querySelector('#adress').value}`,
-      telHouse: `${document.querySelector('#telHouse').value}`,
-      telMob: `${document.querySelector('#telMob').value}`,
-      email: `${document.querySelector('#email').value}`,
-  
-      placeWork: `${document.querySelector('#placeWork').value}`,
-      position: `${document.querySelector('#position').value}`,
-      cityReg: `${document.querySelector('#cityReg').value}`,
-      statusFam: `${document.querySelector('#statusFam').value}`,
-      national: `${document.querySelector('#national').value}`,
-      inval: `${document.querySelector('#inval').value}`,
-      retiree: `${document.querySelector('#retiree').value}`,
-      sum: `${document.querySelector('#sum').value}`,
-    };
+    if (checkValid()) {
 
-    const body = JSON.stringify(userData);
-  
-    console.log('dddddddddddddd', body)
+      const userData = {
+        family: `${document.querySelector('#family').value}`,
+        name: `${document.querySelector('#name').value}`,
+        secondName: `${document.querySelector('#secondName').value}`,
+        dateBorn: `${document.querySelector('#dateBorn').value}`,
+    
+        seriaPasp: `${document.querySelector('#seriaPasp').value}`,
+        numPasp: `${document.querySelector('#numPasp').value}`,
+        whoDone: `${document.querySelector('#whoDone').value}`,
+        dateDone: `${document.querySelector('#dateDone').value}`,
+        idPasp: `${document.querySelector('#idPasp').value}`,
+    
+        placeBorn: `${document.querySelector('#placeBorn').value}`,
+        city: `${document.querySelector('#city').value}`,
+        adress: `${document.querySelector('#adress').value}`,
+        telHouse: `${document.querySelector('#telHouse').value}`,
+        telMob: `${document.querySelector('#telMob').value}`,
+        email: `${document.querySelector('#email').value}`,
+    
+        placeWork: `${document.querySelector('#placeWork').value}`,
+        position: `${document.querySelector('#position').value}`,
+        cityReg: `${document.querySelector('#cityReg').value}`,
+        statusFam: `${document.querySelector('#statusFam').value}`,
+        national: `${document.querySelector('#national').value}`,
+        inval: `${document.querySelector('#inval').value}`,
+        retiree: `${document.querySelector('#retiree').value}`,
+        sum: `${document.querySelector('#sum').value}`,
+      };
 
-    return await fetch('/records', {
-      method: 'post',
+      const body = JSON.stringify(userData);
+    
+      console.log('dddddddddddddd', body)
+
+      return await fetch('/records', {
+        method: 'post',
             headers: {
-                'content-type': 'application/json',
+                  'content-type': 'application/json',
             },
-          body,
+            body,
+      }).then(function (response) {
+        alert(response.status); 
 
-    }).then(function (response) {
-      alert(response.status); 
-
-      return response.json();
-  })
+        return response.json();
+      })
+    }
+    
   }
-  document.querySelector("button[type='submit']").addEventListener('click', getRecords)
 
-  
+  document.querySelector("button[type='submit']").addEventListener('click', getRecords);  
 };
